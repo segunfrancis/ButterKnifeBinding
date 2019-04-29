80b2e5d09a3a62154ca0968c2ea278a725c7c544
segunfrancis <francis4dblues@gmail.com>
2019-04-29 21:40:59 +0100

    Add Second Activity

--- app/src/main/java/com/example/computer/butterknifeandfragments/MainActivity.java
+++ app/src/main/java/com/example/computer/butterknifeandfragments/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.computer.butterknifeandfragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView sampleText;
+
+    @BindView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                RecyclerViewFragment fragment = new RecyclerViewFragment();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.add(R.id.fragment, fragment, "fragment");
+                transaction.commit();
+            }
+        });
     }
 }
